a7bd4808a07b45bef3e76c4aa9dfe79712757caf
Roman291993 <kovalrom2108@gmail.com>
2018-12-08 08:59:01 +0300

    Lesson 03 TaskA complete

--- src/by/it/kovalenko/lesson02/TaskB3.java
+++ src/by/it/kovalenko/lesson02/TaskB3.java
@@ -1,4 +1,4 @@
-package by.it._tasks_.lesson02;
+package by.it.kovalenko.lesson02;
 /*
         Уберите комментарии с части кода, чтобы на экран вывелось сообщение «C Новым Годом».
 
@@ -11,14 +11,14 @@ package by.it._tasks_.lesson02;
 class TaskB3 {
         public static void main(String[] args) {
 //        String s = "C";
-//         System.out.println("Новым Годом");
-//        System.out.println("C Новым")
-//         System.out.println("Годом");
-          System.out.print("C Новым");
-   //       System.out.println("Годом");
+//        System.out.println("Новым Годом");
+//        System.out.println("C Новым");
+//        System.out.println("Годом");
+        System.out.print("C Новым");
+//        System.out.println("Годом");
 //        System.out.println(s);
 //        System.out.print("C Новым!");
-       System.out.print(" ");
+        System.out.print(" ");
         System.out.println("Годом");
     }
 }
